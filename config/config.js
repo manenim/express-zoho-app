8f353466e7d17a997907f917060beef76cf7a204
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-04-22 09:07:51 +0100

    updated scripts

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.uwuvnh5.mongodb.net/?retryWrites=true&w=majority`; 
 
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number( process.env.SERVER_PORT ) : 1337;
+const SERVER_PORT = process.env.PORT || Number( process.env.SERVER_PORT );
 
 export const config = {
      mongo: {
